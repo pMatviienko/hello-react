b082ec456152fafe6b7b42fb2693daa3d38a81d4
Pavlo Matviienko <shturman.p@gmail.com>
2017-04-23 23:52:38 +0300

    - cleaned up to boilerplate

--- app/app.jsx
+++ app/app.jsx
@@ -1,9 +1,8 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var Greeter = require( 'Greeter');
 
 
 ReactDOM.render(
-    <Greeter/>,
+    <h1>Boilerplate App</h1>,
     document.getElementById('app')
 );
