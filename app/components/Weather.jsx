9d506c40e29ba71bbdc097cfff31976c4485c9d4
Pavlo Matviienko <pavlo.matviienko@flixbus.com>
2017-05-22 13:41:58 +0300

    - lecture 41 Stateless components

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -7,28 +7,45 @@ var openWeatherMap = require('OpenWeatherMap');
 var Weather = React.createClass({
     getInitialState: function(){
         return {
-            location: 'N/A',
-            temp: 'N/A'
+            isLoading:false
         }
     },
     handleSearch: function(location){
         var self = this;
+        self.setState({
+            isLoading:true
+        });
 
+        // debugger;
         openWeatherMap.getTemp(location).then(function(temp){
             self.setState({
                 location: location,
-                temp: temp
+                temp: temp,
+                isLoading:false
             })
         },function(errorMessage){
             alert(errorMessage);
+            self.setState({
+                isLoading:false
+            });
         });
     },
     render: function () {
+        var {isLoading, location, temp} = this.state;
+        function renderMessage(){
+            if(isLoading){
+                return <h3>Loading ... </h3>;
+            } else if(location && temp){
+                return <WeatherMessage location={location} temp={temp}/>;
+            } else {
+                return <h3>Enter location to see weather.</h3>
+            }
+        }
         return (
             <div>
                 <h3>Weather component</h3>
                 <WeatherForm onSearch={this.handleSearch}/>
-                <WeatherMessage location={this.state.location} temp={this.state.temp}/>
+                {renderMessage()}
             </div>
         );
     }
--- app/components/Weather/WeatherMessage.jsx
+++ app/components/Weather/WeatherMessage.jsx
@@ -1,13 +1,10 @@
 var React = require('react');
 
-var WeatherMessage = React.createClass({
-    render: function () {
-        return (
-            <div>
-                <h3>Current temp in {this.props.location} is {this.props.temp} degree.</h3>
-            </div>
-        );
-    }
-});
-
+var WeatherMessage = ({temp, location}) => {
+    return (
+        <div>
+            <h3>Current temp in {location} is {temp} degree.</h3>
+        </div>
+    );
+};
 module.exports = WeatherMessage;
